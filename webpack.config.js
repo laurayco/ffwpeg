6086d88aea6e7411e98073e190235b55e5980b15
Lauren Connors <laurayco@outlook.com>
2021-03-23 06:26:42 -0500

    build configs

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    main: path.resolve(__dirname, "obj/main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, 'bin'),
+    filename: "[name]-bundle.js",
+  },
+  resolve: {
+    // Add ".ts" and ".tsx" as resolvable extensions.
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            enforce: "pre",
+            use: ["source-map-loader"],
+        },
+    ],
+  },
+};
